5e6ab41fcb5c63e8b20cc806c77f7b12dc331a91
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-09-30 13:23:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Estimating DICOM intent
+
+Imaging sequences program the scanner to performed a scan for a purpose. We talk about reading **T1-weighted** images or verifying something on a FLAIR. The purpose or **intent** does not map directly to information stored in the DICOM file. Especially for novices it can therefore be difficult to map existing sequence parameter stored in DICOM to the intends like B0, T1, T2*, etc.. 
+
+Instead of experience this project tries to create an algorithm that can perform this assignment from existing DICOM tag values only. No image information is used for this purpose.
+
+## Step 1
+
+Data driven approach: Collect a table of DICOM encoded physical scan parameter for specific imaging modalities like MRI, CT, NM, and US.
+
+One algorithm for each modality should depend on physical scan parameter only. Accidental (entered by a human) measures should be ignored for the prediction step. They may be used for defining intent.
+
+### MRI
+
+Create a folder structure with symbolic links that include scan parameter. Convert the folder structure to a spreadsheet format.
+
+> [!NOTE]
+> Indent: ProtocolName, SequenceName, ContrastBolusAgent
+> Physical scan parameter: ScanningSequence, SequenceVariant, ScanOption, RepetitionTime, EchoTime, ...
+
+```
+sdcm -verbose -folder @mri_tags.sdcm -method link /Volumes/T7/data T7
+cd T7 && ../dir2csv.py -i . -o ../mri.csv
+```
+
+## Step 2
+
+Extract the intent from the accidental fields as a new column. Use a CART model to learn the intent from the physical scan parameters.
+
---
+++ dirs2csv.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import os, json
+import sys, getopt
+
+
+def main(argv):
+   inputdir = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["idir=","ofile="])
+   except getopt.GetoptError:
+      print ('dirs2csv.py -i <inputdir> -o <outputcsv>')
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print ('dirs2csv.py -i <inputdir> -o <outputcsv>')
+         sys.exit()
+      elif opt in ("-i", "--idir"):
+         inputdir = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   if inputdir == '':
+      print("Error: no input dir specified with -i")
+      sys.exit(-1)
+   if not os.path.isdir(inputdir):
+      print("Error: input direction does not exist")
+      sys.exit(-1)
+      
+   if outputfile == '':
+      print("Error: no output file specified with -o")
+      sys.exit(-1)
+
+   header = {}
+   rows = []
+   cache = {}
+   for root, dirs, files in os.walk(inputdir, topdown=False):
+      for name in files:
+         # do each directory only once
+         if root in cache:
+            continue
+         else:
+            cache[root] = True
+         s = root.split("/")
+         v = {}
+         for e in s:
+            es = e.split("_")
+            header[es[0]] = True
+            v[es[0]] = "_".join(es[1:])
+         rows.append(v)
+   if outputfile.endswith(".json"):
+      with open(outputfile,"w") as f:
+         json.dump(rows,f)
+      sys.exit(0)
+   else:
+      # assume we have a csv file to export
+      import csv
+      with open(outputfile, 'w', newline='') as csvfile:
+         writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+         writer.writerow(header.keys())
+         for row in rows:
+            r = []
+            for h in header.keys():
+               r.append(row[h])
+            writer.writerow(r)
+         
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
